0f984b93334bcd6d7bf715e5a6123d13dd6af95d
Sari Shehada <sarishehada@gmail.com>
2025-08-06 19:54:18 +0400

    Added EntityBase and DomainUser Entity

---
+++ Zaxon.CustomsStockManagement.Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Zaxon.CustomsStockManagement.Domain.Entities.Base;
+
+public class EntityBase
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Zaxon.CustomsStockManagement.Domain/Entities/DomainUser.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Zaxon.CustomsStockManagement.Domain.Entities.Base;
+
+namespace Zaxon.CustomsStockManagement.Domain.Entities;
+
+
+//TODO: Re-think
+public class DomainUser : EntityBase
+{
+
+    [MaxLength(64)]
+    public required string Username { get; set; }
+
+    [MaxLength(64)]
+    public required string PasswordHash { get; set; }
+
+    public required string FullName { get; set; }
+
+    public bool ShouldResetPasswordUponLogin { get; set; } = true;
+    public bool CanAddCustomsDocument { get; set; } = false;
+    public bool CanViewStockChanges { get; set; } = false;
+    public bool CanAddUsers { get; set; } = false;
+    public bool CanModifyUsers { get; set; } = false;
+}
