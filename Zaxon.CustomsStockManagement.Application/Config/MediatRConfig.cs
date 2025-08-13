e8c1aaa54eb618badf538c29984afcd5bdd0226d
Sari Shehada <sarishehada@gmail.com>
2025-08-13 19:14:25 +0400

    Added & Configured MediatR

---
+++ Zaxon.CustomsStockManagement.Application/Config/MediatRConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Zaxon.CustomsStockManagement.Application.Config;
+
+public static class MediatRConfig
+{
+    public static IServiceCollection ConfigureMediatR(this IServiceCollection services)
+    {
+        return services.
+            AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+    }
+}
--- Zaxon.CustomsStockManagement.Application/DependencyInjection.cs
+++ Zaxon.CustomsStockManagement.Application/DependencyInjection.cs
@@ -1,9 +1,13 @@
 ﻿namespace Zaxon.CustomsStockManagement.Application;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Zaxon.CustomsStockManagement.Application.Config;
+
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        return services;
+        return services
+            .ConfigureMediatR();
     }
 }
