b4288ee51f3cebd336a535ec75c026f13872230b
Sari Shehada <sarishehada@gmail.com>
2025-08-13 19:35:47 +0400

    Added IDateTimeProcider & IJwtHandler

---
+++ Zaxon.CustomsStockManagement.Application/Common/Auth/IJwtTokenHandler.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Claims;
+
+namespace Zaxon.CustomsStockManagement.Application.Common.Auth;
+
+public interface IJwtTokenHandler
+{
+    string GenerateToken(List<ClaimsPrincipal> claims);
+}
---
+++ Zaxon.CustomsStockManagement.Application/Common/Services/IDateTimeProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace Zaxon.CustomsStockManagement.Application.Common.Services;
+
+public interface IDateTimeProvider
+{
+    DateTime UtcNow { get; }
+}
