ade2879084b926b6f72b24b43075eb81a10209a1
Sari Shehada <sarishehada@gmail.com>
2025-08-15 11:36:03 +0400

    Added LoginCommandHanlder Intermediate Impl

--- Zaxon.CustomsStockManagement.Application/Repositories/IUserRepository.cs
+++ Zaxon.CustomsStockManagement.Application/Repositories/IUserRepository.cs
@@ -4,6 +4,6 @@ namespace Zaxon.CustomsStockManagement.Application.Repositories;
 
 public interface IUserRepository
 {
-    Task<DomainUser> GetUserByUsernameAsync(string username);
+    Task<DomainUser?> GetUserByUsernameAsync(string username);
     Task UpdateUserLastLoginDate(Guid id, DateTime dateTime);
 }
--- Zaxon.CustomsStockManagement.Application/Services/Auth/Commands/LoginCommandHandler.cs
+++ Zaxon.CustomsStockManagement.Application/Services/Auth/Commands/LoginCommandHandler.cs
@@ -1,11 +1,45 @@
 ﻿using MediatR;
+using Zaxon.CustomsStockManagement.Application.Common.Auth;
+using Zaxon.CustomsStockManagement.Application.Common.Services;
+using Zaxon.CustomsStockManagement.Application.Repositories;
 
 namespace Zaxon.CustomsStockManagement.Application.Services.Auth.Commands;
 
-public class LoginCommandHandler : IRequestHandler<LoginCommand, AuthResult>
+public class LoginCommandHandler
+    (IUserRepository userRepository,
+    IPasswordManager passwordManager,
+    IJwtTokenHandler jwtTokenHandler,
+    IUserClaimsHandler userClaimsHandler,
+    IDateTimeProvider dateTimeProvider)
+    : IRequestHandler<LoginCommand, AuthResult>
 {
+
     public async Task<AuthResult> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        //validate the user does exist
+        var user = await userRepository.GetUserByUsernameAsync(request.Username);
+        if (user is null || !passwordManager.ValidatePasswordAgainstHash(request.Password, user.PasswordHash))
+        {
+            //TODO: Change later
+            throw new Exception("Invalid Login Credentials");
+        }
+
+        //Update user last login date to now
+        await userRepository.UpdateUserLastLoginDate(user.Id, dateTimeProvider.UtcNow);
+
+        //Generate token if credentials are correct
+        var token = jwtTokenHandler.GenerateToken(
+            userClaimsHandler.GenerateClaimsList(user));
+
+        return new AuthResult()
+        {
+            AccessToken = token,
+            User = new UserDto()
+            {
+                UserId = user.Id,
+                Username = user.Username,
+                FullName = user.FullName,
+            },
+        };
     }
 }
