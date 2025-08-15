100cbfc770a15ce2eb12c046311b8628569190dd
Sari Shehada <sarishehada@gmail.com>
2025-08-15 11:41:16 +0400

    Implemented JwtTokenHanlder

---
+++ Zaxon.CustomsStockManagement.Infrastructure/Common/Auth/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Zaxon.CustomsStockManagement.Infrastructure.Common.Auth;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public string SecurityKey { get; set; }
+    public int TokenValidityInHours { get; set; }
+}
--- Zaxon.CustomsStockManagement.Infrastructure/Common/Auth/JwtTokenHandler.cs
+++ Zaxon.CustomsStockManagement.Infrastructure/Common/Auth/JwtTokenHandler.cs
@@ -1,12 +1,43 @@
-﻿using System.Security.Claims;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Zaxon.CustomsStockManagement.Application.Common.Auth;
+using Zaxon.CustomsStockManagement.Application.Common.Services;
 
 namespace Zaxon.CustomsStockManagement.Infrastructure.Common.Auth;
 
 public class JwtTokenHandler : IJwtTokenHandler
 {
+    private readonly IDateTimeProvider dateTimeProvider;
+    private readonly JwtSettings jwtSettings;
+
+    private readonly SymmetricSecurityKey key;
+    private readonly string securityAlgorithm = SecurityAlgorithms.HmacSha256;
+
+
+    public JwtTokenHandler(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtOptions)
+    {
+        this.dateTimeProvider = dateTimeProvider;
+        jwtSettings = jwtOptions.Value;
+        key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.SecurityKey));
+    }
+
+
+
+
     public string GenerateToken(List<Claim> claims)
     {
-        throw new NotImplementedException();
+        var signingCredentials = new SigningCredentials(key, securityAlgorithm);
+
+        var securityToken = new JwtSecurityToken(
+            claims: claims,
+            signingCredentials: signingCredentials,
+            issuer: jwtSettings.Issuer,
+            audience: jwtSettings.Audience,
+            expires: dateTimeProvider.UtcNow.AddHours(jwtSettings.TokenValidityInHours));
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
     }
 }
